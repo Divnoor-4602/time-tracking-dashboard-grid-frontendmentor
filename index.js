1a2c1b7dc0e2e66461682d74e43903b125301123
Divnoor <divnoorsingh4602@gmail.com>
2023-12-25 19:57:18 +0530

    final commit complete project

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
 import data from "./data.json" assert { type: "json" };
 // importing the data.json file to our js file to use the data
+
+function changeButtonColor(buttonId) {
+  $(".toggle-views").removeClass("button-press");
+  $("#" + buttonId).addClass("button-press");
+}
+
+function valueFill(activityCardTitle, current, previous, viewType) {
+  let initialStatement;
+  switch (viewType) {
+    case "daily":
+      initialStatement = "Yesterday - ";
+      break;
+    case "weekly":
+      initialStatement = "Last week - ";
+      break;
+    case "monthly":
+      initialStatement = "Last month - ";
+      break;
+    default:
+      break;
+  }
+
+  console.log(initialStatement);
+
+  $("#" + activityCardTitle + ".activities")
+    .children()
+    .children(".activity-card-row-2")
+    .children(".activity-duration")
+    .children(".curr")
+    .text(current);
+
+  $("#" + activityCardTitle + ".activities")
+    .children()
+    .children(".activity-card-row-2")
+    .children(".activity-last-weekly-score")
+    .text(initialStatement + previous + "hrs");
+}
+
+function getData(dataType) {
+  data.forEach((typeOfActivity) => {
+    let activityTitle = typeOfActivity.title.toLowerCase();
+    let activityHoursToSendCurrent =
+      typeOfActivity.timeframes[dataType].current;
+    let activityHoursToSendPrevious =
+      typeOfActivity.timeframes[dataType].previous;
+
+    valueFill(
+      activityTitle,
+      activityHoursToSendCurrent,
+      activityHoursToSendPrevious,
+      dataType
+    );
+  });
+}
+
+$(".toggle-views").on("click", function (event) {
+  let currentButtonView = event.target.id;
+  changeButtonColor(currentButtonView);
+  getData(currentButtonView);
+});
